ddd77ff7e0d3056b923b363146212309a14bb121
NghiaNT <nghiant.hust@gmail.com>
2020-08-05 09:33:47 +0700

    bai19-prop-types

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,24 +1,91 @@
 import React, { Component } from 'react';
 import './App.css';
 import TodoItem from './components/todoItem';
+import CompleteImg from './img/completeAll.svg';
 
 class App extends Component {
   constructor (){
     super();
-    this.todoItems = [
-      {title: 'Di da bong', isComplete: true}, 
-      {title: 'Hoc Code Web', isComplete: false},
-      {title: 'Tap the duc', isComplete: true} 
-    ]
+    this.state = {
+      todoItems:[
+        {title: 'Hoc Code Web', isComplete: false},
+        {title: 'Tap the duc', isComplete: true} 
+      ]
+    };
+    this.onKeyUp = this.onKeyUp.bind(this);
+    this.onChange = this.onChange.bind(this);
+
   }
 
+  onItemClicked(item){
+    
+    return (event) => {
+      const isComplete = item.isComplete;
+      const {todoItems} = this.state;
+      const index = todoItems.indexOf(item);
+      
+      this.setState({
+        todoItems: [
+          ...todoItems.slice(0,index),
+          {
+            ...item,isComplete: !isComplete
+          },
+          ...todoItems.slice(index+1)
+        ]
+      })
+      // this.setState({
+      //   todoItems: [...todoItems.map((item)=>{
+      //     if(todoItems.indexOf(item) === index){
+      //       item.isComplete = !isComplete
+      //     }
+      //     return item
+      //   })]
+      // })
+      
+    }
+  }
+
+  onKeyUp(event){
+    // console.log(event.keyCode)
+    if(event.keyCode === 13){
+      const value = event.target.value;
+      const {todoItems} = this.state;
+      this.setState({
+        newItem: '',
+        todoItems: [
+          {
+            title: value,
+            isComplete: false
+          },
+          ...todoItems
+        ],
+      })
+      
+    }    
+  }
+
+  onChange(event){
+    this.setState({
+      newItem: event.target.value
+    })
+  }
+  
   render(){
+    const {todoItems,newItem} = this.state;
     return (
       <div className="App">
-          {this.todoItems.length > 0 && this.todoItems.map((item,index) => 
-            <TodoItem key={index} item={item}/>)}
-
-          {this.todoItems.length === 0 && 'Nothing here'}
+          <div className="Header">
+            <img src = {CompleteImg} alt="completeAll-icon" width={32} height={32}/>
+            <input type="text" 
+              placeholder="What needs to be done?" 
+              onKeyUp={this.onKeyUp}
+              value={newItem}
+              onChange={this.onChange}
+            />
+          </div>
+          {todoItems.length > 0 && todoItems.map((item,index) => 
+            <TodoItem key={index} item={item} onClick={this.onItemClicked(item)}/>)}
+          {todoItems.length === 0 && 'Nothing here'}
       </div>
     );
   }
--- todo-list/src/components/todoItem.js
+++ todo-list/src/components/todoItem.js
@@ -1,19 +1,37 @@
 import React, {Component} from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+
 import './todoItem.css';
+import checkImg from '../img/check.svg';
+import checkCompleteImg from '../img/checkComplete.svg';
 
 class TodoItem extends Component {
+
     render(){
-        const {item} = this.props;
-        let className = 'TodoItem';
+        const {item, onClick} = this.props;
+        let url = checkImg;
         if(item.isComplete){
-            className += ' TodoItem-complete';
+            url = checkCompleteImg;
         }
         return(
-            <div className={className}>
-                <p>{this.props.item.title}</p>
+            <div className={classNames(
+                'TodoItem',
+                {'TodoItem-complete' : item.isComplete}
+                )}>
+              <img onClick={onClick} src = {url} alt="check-icon" width={32} height={32}/>  
+              <p>{this.props.item.title}</p>
             </div>
         );
     }
+}
+
+TodoItem.propTypes = {
+    item: PropTypes.shape({
+        title: PropTypes.string,
+        isComplete: PropTypes.bool.isRequired
+    }),
+    onClick: PropTypes.func
 }
 
 export default TodoItem;
